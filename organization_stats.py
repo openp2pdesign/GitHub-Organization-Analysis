b8c009e5a4830070903aa93d5ed5f697075dd60d
Massimo Menichinelli <info@openp2pdesign.org>
2013-12-19 15:02:52 +0100

    Remove graph analysis from organization_stats.py in order to build the new version

--- organization_stats.py
+++ organization_stats.py
@@ -1,5 +1,5 @@
 #
-# Social analysis of an Organization repository in GitHub
+# General statistics of an Organization repository in GitHub
 #
 # Author: Massimo Menichinelli
 # Homepage: http://www.openp2pdesign.org
@@ -7,21 +7,19 @@
 #
 # Requisite: 
 # install pyGithub with pip install PyGithub
-# install NetworkX with pip install networkx
+# install Matplotlib with pip install matplotlib
 #
 # PyGitHub documentation can be found here: 
 # https://github.com/jacquev6/PyGithub
 #
 
 from github import Github
-import networkx as nx
 import getpass
 
-from repoanalysis import analyse_repo
+from repostats import analyse_repo
 
 # Variables for the whole program
 
-graph = nx.MultiDiGraph()
 issue = {}
 issue = {0:{"author":"none", "comments":{}}}
 commits = {0:{"commit","sha"}}
@@ -62,41 +60,6 @@ if __name__ == "__main__":
         print "---------"
         print "NOW ANALYSING:", repo.name
         b = org.get_repo(repo.name)
-        analyse_repo(b,graph)
+        analyse_repo(b)
     
-    # Getting rid of the node "None", it was used to catch the errors of users that are NoneType
-    graph.remove_node('None')
-    
-    print ""
-    print "NODES..."
-    print graph.nodes()
-    print ""
-    print "EDGES..."
-    print graph.edges()
-    print ""
-    
-    print "Converting multiple edges to weighted edges..."
-    print ""
-    
-    graph2 = nx.DiGraph()
-    
-    for j in list(graph.nodes_iter(data=True)):
-        # copying all the nodes with their attributes
-        graph2.add_node(j[0],collaborator=j[1]["collaborator"],contributor=j[1]["contributor"],owner=j[1]["owner"],watcher=j[1]["watcher"])
-        
-    
-    for j in list(graph.edges_iter(data=True)):
-        subject_id = j[0]
-        object_id = j[1]
-        if graph.has_edge(subject_id, object_id) and graph2.has_edge(subject_id, object_id):
-            graph2[subject_id][object_id]['weight'] += 1
-        elif graph.has_edge(subject_id, object_id) and not graph2.has_edge(subject_id, object_id):
-            graph2.add_edge(subject_id, object_id, weight=1)
-            
-    print "EDGES..."
-    print graph2.edges()
-    print ""
-
-    print "Saving the network..."
-    nx.write_gexf(graph2, username+"_allrepositories_social_interactions_analysis.gexf")
     print "Done. Saved as "+username+"_allrepositories_social_interactions_analysis.gexf"
