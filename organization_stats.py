b0a19a057af50d77e4bbc12e356b1f75c79b4716
Massimo Menichinelli <info@openp2pdesign.org>
2013-12-28 18:03:24 -0600

    First sketch of loading data

--- organization_stats.py
+++ organization_stats.py
@@ -135,28 +135,21 @@ if __name__ == "__main__":
     
     # All activity through time, by person
     
-    fig, ax = plt.subplots()
-    ax.plot(r.date, r.adj_close)
+    # Define data
+    x = [1,2,3,4]
+    y = [20, 21, 20.5, 20.8]
     
-    # format the ticks
-    ax.xaxis.set_major_locator(years)
-    ax.xaxis.set_major_formatter(yearsFmt)
-    ax.xaxis.set_minor_locator(months)
+    print events["openp2pdesign"]
+    print "TOTAL ACTIVITY:", len(events["openp2pdesign"])
+    for j in events["openp2pdesign"]:
+        print j
+        print "TIME:",events["openp2pdesign"][j]["time"]
+        print "TYPE:",events["openp2pdesign"][j]["type"]
     
-    datemin = datetime.date(r.date.min().year, 1, 1)
-    datemax = datetime.date(r.date.max().year+1, 1, 1)
-    ax.set_xlim(datemin, datemax)
+    # Plot data
+    plt.plot(x, y)
     
-    # format the coords message box
-    def price(x): return '$%1.2f'%x
-    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
-    ax.format_ydata = price
-    ax.grid(True)
-    
-    # rotates and right aligns the x labels, and moves the bottom of the
-    # axes up to make room for them
-    fig.autofmt_xdate()
-    
+    # Show plot
     plt.show()
     
     # All activity trough time, all persons
