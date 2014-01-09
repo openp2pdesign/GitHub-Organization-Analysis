7300a65b7794c10b0ba60d2d5dda8c308509fa7a
Massimo Menichinelli <info@openp2pdesign.org>
2014-01-10 00:17:30 +0100

    Add first sketch of loading json file

--- organization_stats.py
+++ organization_stats.py
@@ -41,120 +41,127 @@ events = {}
 if __name__ == "__main__":
     print "Simple statistics of your GitHub Organization"
     print ""
-    userlogin = raw_input("Login: Enter your username: ")
-    password = getpass.getpass("Login: Enter yor password: ")
-    username = raw_input("Enter the username you want to analyse: ")
+    choice = raw_input("Do you want to mine GitHub or to load an existing .json file?(yes - no) ")
     print ""
-    g = Github( userlogin, password )
-    
-    
-    print "ORGANIZATIONS:"
-    for i in g.get_user(username).get_orgs():
-        print "-", i.login
-    print ""
-    
-    org_to_mine = raw_input("Enter the name of the Organization you want to analyse: ")
-    print ""
-    
-    org = g.get_organization(org_to_mine)
-    
-    # Create a directory with the name of the organization for saving analysis
-    directory = org_to_mine+"-stats"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    
-    print org.login,"has",org.public_repos, "repositories."
-    
-    print ""
-    
-    for repo in org.get_repos():
-        print "-",repo.name
-    
-    print ""    
-    
-    # Get all users in the organization by each repository
-    # Get all the roles for each user
-    for repo in org.get_repos():
-        print "---------"
-        print "NOW ANALYSING:", repo.name
-        repository = org.get_repo(repo.name)
-
-        print "-----"
-        print "WATCHERS:",repository.watchers
+    if choice == "yes" or choice == "Yes" or choice == "YES":
+        userlogin = raw_input("Login: Enter your username: ")
+        password = getpass.getpass("Login: Enter yor password: ")
+        username = raw_input("Enter the username you want to analyse: ")
         print ""
-        for i in repository.get_stargazers():
-            if i != None:
-                print "-",i.login
-                if i.login not in users:
-                    users[i.login] = {}
-                    users[i.login]["watcher"]="Yes"
-                else:
-                    users[i.login]["watcher"]="Yes" 
-            else:
-                users["None"]["watcher"]="Yes" 
-        print "-----"
-        print "COLLABORATORS"
+        g = Github( userlogin, password )
+        
+        
+        print "ORGANIZATIONS:"
+        for i in g.get_user(username).get_orgs():
+            print "-", i.login
         print ""
-        for i in repository.get_collaborators():
-            if i != None:
-                print "-",i.login
-                if i.login not in users:
-                    users[i.login] = {}
-                    users[i.login]["collaborator"]="Yes"
-                else:
-                    users[i.login]["collaborator"]="Yes"
-            else:
-                users["None"]["collaborator"]="Yes"
-        print "-----"
-        print "CONTRIBUTORS"
+        
+        org_to_mine = raw_input("Enter the name of the Organization you want to analyse: ")
         print ""
-        for i in repository.get_contributors():
-            if i.login != None:
-                print "-", i.login
-                if i.login not in users:
+        
+        org = g.get_organization(org_to_mine)
+        
+        # Create a directory with the name of the organization for saving analysis
+        directory = org_to_mine+"-stats"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        print org.login,"has",org.public_repos, "repositories."
+        
+        print ""
+        
+        for repo in org.get_repos():
+            print "-",repo.name
+        
+        print ""    
+        
+        # Get all users in the organization by each repository
+        # Get all the roles for each user
+        for repo in org.get_repos():
+            print "---------"
+            print "NOW ANALYSING:", repo.name
+            repository = org.get_repo(repo.name)
+    
+            print "-----"
+            print "WATCHERS:",repository.watchers
+            print ""
+            for i in repository.get_stargazers():
+                if i != None:
+                    print "-",i.login
+                    if i.login not in users:
                         users[i.login] = {}
+                        users[i.login]["watcher"]="Yes"
+                    else:
+                        users[i.login]["watcher"]="Yes" 
+                else:
+                    users["None"]["watcher"]="Yes" 
+            print "-----"
+            print "COLLABORATORS"
+            print ""
+            for i in repository.get_collaborators():
+                if i != None:
+                    print "-",i.login
+                    if i.login not in users:
+                        users[i.login] = {}
+                        users[i.login]["collaborator"]="Yes"
+                    else:
+                        users[i.login]["collaborator"]="Yes"
+                else:
+                    users["None"]["collaborator"]="Yes"
+            print "-----"
+            print "CONTRIBUTORS"
+            print ""
+            for i in repository.get_contributors():
+                if i.login != None:
+                    print "-", i.login
+                    if i.login not in users:
+                            users[i.login] = {}
+                            users[i.login]["contributor"]="Yes"
+                    else:
                         users[i.login]["contributor"]="Yes"
                 else:
-                    users[i.login]["contributor"]="Yes"
-            else:
-                users["None"]["contributor"]="Yes"
-                
-        # Check the attributes of every node, and add a "No" when it is not present
-        for i in users:
-            if "owner" not in users[i]:
-                users[i]["owner"] = "No"
-            if "contributor" not in users[i]:
-                users[i]["contributor"] = "No"               
-            if "collaborator" not in users[i]:
-                users[i]["collaborator"] = "No"
-            if "watcher" not in users[i]:
-                users[i]["watcher"] = "No"
-
-        
-    # Get all events in the organization
-    # Description: http://developer.github.com/v3/activity/events/types/
-    print "------"
-    print "EVENTS"
-    print
-    lastevent = []
-    for j in org.get_events():
-        print "-- ",j.type,"event by",j.actor.login,"from repo:",j.repo.name, "at",j.created_at, "ID:",j.id
-        if j.actor.login not in events:
-            events[j.actor.login] = {}        
-        events[j.actor.login][j.id] = {}
-        events[j.actor.login][j.id]["time"] = j.created_at
-        events[j.actor.login][j.id]["type"] = j.type
-        events[j.actor.login][j.id]["repo"] = j.repo.name
-        lastevent.append(j.created_at)
+                    users["None"]["contributor"]="Yes"
+                    
+            # Check the attributes of every node, and add a "No" when it is not present
+            for i in users:
+                if "owner" not in users[i]:
+                    users[i]["owner"] = "No"
+                if "contributor" not in users[i]:
+                    users[i]["contributor"] = "No"               
+                if "collaborator" not in users[i]:
+                    users[i]["collaborator"] = "No"
+                if "watcher" not in users[i]:
+                    users[i]["watcher"] = "No"
     
-    # Save content as json file
-    save_events = copy.deepcopy(events)
-    for i in save_events:
-        for k in save_events[i]:
-            save_events[i][k]["time"] = str(save_events[i][k]["time"])
-    with open(directory+"/"+"events.json", 'w') as outfile:
-        json.dump(save_events, outfile)
-    # convert it back to python datetime: datetime.strptime('2013-12-17 22:14:12', '%Y-%m-%d %H:%M:%S')
+            
+        # Get all events in the organization
+        # Description: http://developer.github.com/v3/activity/events/types/
+        print "------"
+        print "EVENTS"
+        print
+        lastevent = []
+        for j in org.get_events():
+            print "-- ",j.type,"event by",j.actor.login,"from repo:",j.repo.name, "at",j.created_at, "ID:",j.id
+            if j.actor.login not in events:
+                events[j.actor.login] = {}        
+            events[j.actor.login][j.id] = {}
+            events[j.actor.login][j.id]["time"] = j.created_at
+            events[j.actor.login][j.id]["type"] = j.type
+            events[j.actor.login][j.id]["repo"] = j.repo.name
+            lastevent.append(j.created_at)
+        
+        # Save content as json file
+        save_events = copy.deepcopy(events)
+        for i in save_events:
+            for k in save_events[i]:
+                save_events[i][k]["time"] = str(save_events[i][k]["time"])
+        with open(directory+"/"+"events.json", 'w') as outfile:
+            json.dump(save_events, outfile)
+        # convert it back to python datetime: datetime.strptime('2013-12-17 22:14:12', '%Y-%m-%d %H:%M:%S')
+    
+    elif choice == "no" or choice == "No" or choice == "NO":
+        pause = raw_input("Copy your file events.json in the current directory and press Enter")
+        print "Loading file events.json"
     
     # ................................................................................................
     # Separate activities by repository ..............................................................
